e4b051d539fcb7251557fd38833a6604be7848ca
Carlos O'Ryan <coryan@google.com>
2026-05-04 12:05:26 -0400

    feat(gax): ProtoJSON for scalars (#86)
    In ProtoJSON, we must be able to deserialize scalars (booleans,
    integers, and floating point numbers) from their representation as
    string JSON values.

--- Sources/GoogleCloudGax/ProtoJSONDecoder.swift
+++ Sources/GoogleCloudGax/ProtoJSONDecoder.swift
@@ -68,14 +68,68 @@ extension InternalDecoder: Decoder {
   }
 
   public func singleValueContainer() throws -> any SingleValueDecodingContainer {
-    // TODO(https://github.com/googleapis/librarian/issues/5260) - wrap for special values
-    return try self.impl.singleValueContainer()
+    return InternalSingleValueContainer(try self.impl.singleValueContainer())
   }
 
   public func unkeyedContainer() throws -> any UnkeyedDecodingContainer {
     let impl = try self.impl.unkeyedContainer()
     return InternalUnkeyedDecodingContainer(impl)
   }
+}
+
+fileprivate struct InternalSingleValueContainer {
+  let impl: any SingleValueDecodingContainer
+  init(_ impl: any SingleValueDecodingContainer) { self.impl = impl }
+
+  func decodeGeneric<T: LosslessStringConvertible & Decodable>(_ type: T.Type) throws -> T {
+    if let value = try? impl.decode(type) {
+      return value
+    }
+    if let string = try? impl.decode(String.self), let value = T(string) {
+      return value
+    }
+    throw DecodingError.dataCorruptedError(
+      in: self, debugDescription: "Expected value of type \(type).")
+  }
+}
+
+extension InternalSingleValueContainer: SingleValueDecodingContainer {
+  var codingPath: [any CodingKey] { impl.codingPath }
+
+  func decodeNil() -> Bool { impl.decodeNil() }
+  func decode(type: String.Type) throws -> String { try impl.decode(type) }
+
+  func decode(_ type: Bool.Type) throws -> Bool {
+    if let value = try? impl.decode(type) {
+      return value
+    }
+    if let value = try? impl.decode(String.self) {
+      if value == "true" {
+        return true
+      }
+      if value == "false" {
+        return false
+      }
+    }
+    throw DecodingError.dataCorruptedError(
+      in: self, debugDescription: "Expected value of type Bool.")
+  }
+  func decode(_ type: Int.Type) throws -> Int { try decodeGeneric(type) }
+  func decode(_ type: UInt.Type) throws -> UInt { try decodeGeneric(type) }
+  func decode(_ type: Int8.Type) throws -> Int8 { try decodeGeneric(type) }
+  func decode(_ type: UInt8.Type) throws -> UInt8 { try decodeGeneric(type) }
+  func decode(_ type: Int16.Type) throws -> Int16 { try decodeGeneric(type) }
+  func decode(_ type: UInt16.Type) throws -> UInt16 { try decodeGeneric(type) }
+  func decode(_ type: Int32.Type) throws -> Int32 { try decodeGeneric(type) }
+  func decode(_ type: UInt32.Type) throws -> UInt32 { try decodeGeneric(type) }
+  func decode(_ type: Int64.Type) throws -> Int64 { try decodeGeneric(type) }
+  func decode(_ type: UInt64.Type) throws -> UInt64 { try decodeGeneric(type) }
+  func decode(_ type: Int128.Type) throws -> Int128 { try decodeGeneric(type) }
+  func decode(_ type: UInt128.Type) throws -> UInt128 { try decodeGeneric(type) }
+  func decode(_ type: Float.Type) throws -> Float { try decodeGeneric(type) }
+  func decode(_ type: Double.Type) throws -> Double { try decodeGeneric(type) }
+
+  func decode<T: Decodable>(_ type: T.Type) throws -> T { try impl.decode(type) }
 }
 
 fileprivate struct InternalKeyedContainer<K: CodingKey> {
--- Tests/ProtoJSONDecoder.swift
+++ Tests/ProtoJSONDecoder.swift
@@ -91,6 +91,76 @@ import Testing
     #expect(got == MessageWithScalars())
   }
 
+  @Test(
+    "decode scalars from strings",
+    arguments: [
+      (#"{}"#, MessageWithAllScalars()),
+      (#"{"fieldBool":   "true"  }"#, MessageWithAllScalars().with { $0.fieldBool = true }),
+      (#"{"fieldBool":   "false" }"#, MessageWithAllScalars().with { $0.fieldBool = false }),
+      (#"{"fieldInt8":   "42"    }"#, MessageWithAllScalars().with { $0.fieldInt8 = 42 }),
+      (#"{"fieldInt16":  "42"    }"#, MessageWithAllScalars().with { $0.fieldInt16 = 42 }),
+      (#"{"fieldInt32":  "42"    }"#, MessageWithAllScalars().with { $0.fieldInt32 = 42 }),
+      (#"{"fieldInt64":  "42"    }"#, MessageWithAllScalars().with { $0.fieldInt64 = 42 }),
+      (#"{"fieldUInt8":  "42"    }"#, MessageWithAllScalars().with { $0.fieldUInt8 = 42 }),
+      (#"{"fieldUInt16": "42"    }"#, MessageWithAllScalars().with { $0.fieldUInt16 = 42 }),
+      (#"{"fieldUInt32": "42"    }"#, MessageWithAllScalars().with { $0.fieldUInt32 = 42 }),
+      (#"{"fieldUInt64": "42"    }"#, MessageWithAllScalars().with { $0.fieldUInt64 = 42 }),
+      (#"{"fieldFloat":  "42"    }"#, MessageWithAllScalars().with { $0.fieldFloat = 42 }),
+      (#"{"fieldDouble": "42"    }"#, MessageWithAllScalars().with { $0.fieldDouble = 42 }),
+    ])
+  func decodeScalarFromString(input: String, want: MessageWithAllScalars) throws {
+    let decoder = ProtoJSONDecoder()
+    let got = try decoder.decode(MessageWithAllScalars.self, from: input.data(using: .utf8)!)
+    #expect(got == want)
+  }
+
+  @Test(
+    "decode scalars from bad strings",
+    arguments: [
+      #"{"fieldBool":   "bad" }"#,
+      #"{"fieldBool":   "bad" }"#,
+      #"{"fieldInt8":   "bad" }"#,
+      #"{"fieldInt16":  "bad" }"#,
+      #"{"fieldInt32":  "bad" }"#,
+      #"{"fieldInt64":  "bad" }"#,
+      #"{"fieldUInt8":  "bad" }"#,
+      #"{"fieldUInt16": "bad" }"#,
+      #"{"fieldUInt32": "bad" }"#,
+      #"{"fieldUInt64": "bad" }"#,
+      #"{"fieldFloat":  "bad" }"#,
+      #"{"fieldDouble": "bad" }"#,
+    ])
+  func decodeScalarFromBadString(input: String) throws {
+    let decoder = ProtoJSONDecoder()
+    let error = #expect(throws: DecodingError.self) {
+      try decoder.decode(MessageWithAllScalars.self, from: input.data(using: .utf8)!)
+    }
+    #expect({ if case .dataCorrupted = error { true } else { false } }())
+  }
+
+  @Test(
+    "decode scalars from numbers",
+    arguments: [
+      (#"{}"#, MessageWithAllScalars()),
+      (#"{"fieldBool":   true  }"#, MessageWithAllScalars().with { $0.fieldBool = true }),
+      (#"{"fieldBool":   false }"#, MessageWithAllScalars().with { $0.fieldBool = false }),
+      (#"{"fieldInt8":   42    }"#, MessageWithAllScalars().with { $0.fieldInt8 = 42 }),
+      (#"{"fieldInt16":  42    }"#, MessageWithAllScalars().with { $0.fieldInt16 = 42 }),
+      (#"{"fieldInt32":  42    }"#, MessageWithAllScalars().with { $0.fieldInt32 = 42 }),
+      (#"{"fieldInt64":  42    }"#, MessageWithAllScalars().with { $0.fieldInt64 = 42 }),
+      (#"{"fieldUInt8":  42    }"#, MessageWithAllScalars().with { $0.fieldUInt8 = 42 }),
+      (#"{"fieldUInt16": 42    }"#, MessageWithAllScalars().with { $0.fieldUInt16 = 42 }),
+      (#"{"fieldUInt32": 42    }"#, MessageWithAllScalars().with { $0.fieldUInt32 = 42 }),
+      (#"{"fieldUInt64": 42    }"#, MessageWithAllScalars().with { $0.fieldUInt64 = 42 }),
+      (#"{"fieldFloat":  42    }"#, MessageWithAllScalars().with { $0.fieldFloat = 42 }),
+      (#"{"fieldDouble": 42    }"#, MessageWithAllScalars().with { $0.fieldDouble = 42 }),
+    ])
+  func decodeScalarFromNumber(input: String, want: MessageWithAllScalars) throws {
+    let decoder = ProtoJSONDecoder()
+    let got = try decoder.decode(MessageWithAllScalars.self, from: input.data(using: .utf8)!)
+    #expect(got == want)
+  }
+
   // Only test the types that appear in sidekick messages. `Int` and `UInt` may be used with enums.
   struct MessageWithOptionals: Decodable, Equatable {
     var fieldBool: Bool? = nil
@@ -111,7 +181,6 @@ import Testing
       return copy
     }
   }
-
   struct MessageWithMap: Decodable, Equatable {
     var stringField: String = String()
     var mapField: [String: String] = [:]
@@ -127,5 +196,24 @@ import Testing
     var dataField: Data = Data()
     var intField: Int64 = Int64()
     var boolField: Bool = false
+  }
+
+  struct MessageWithAllScalars: Decodable, Equatable {
+    func with(_ config: (inout Self) -> Void) -> Self {
+      var copy = self
+      config(&copy)
+      return copy
+    }
+    var fieldBool: Bool = Bool()
+    var fieldInt8: Int8 = Int8()
+    var fieldUInt8: UInt8 = UInt8()
+    var fieldInt16: Int16 = Int16()
+    var fieldUInt16: UInt16 = UInt16()
+    var fieldInt32: Int32 = Int32()
+    var fieldUInt32: UInt32 = UInt32()
+    var fieldInt64: Int64 = Int64()
+    var fieldUInt64: UInt64 = UInt64()
+    var fieldFloat: Float = Float()
+    var fieldDouble: Double = Double()
   }
 }
