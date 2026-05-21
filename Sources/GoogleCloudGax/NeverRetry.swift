5cf5cbc653fca3f9ba8760e9a1653ec464091527
Carlos O'Ryan <coryan@google.com>
2026-05-21 16:15:48 -0400

    feat(gax): introduce `NeverRetry` retry policy (#174)

---
+++ Sources/GoogleCloudGax/NeverRetry.swift
@@ -0,0 +1,32 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+
+/// A retry policy that never retries.
+///
+/// This policy may be useful when:
+/// - you want to avoid retrying a particular method
+/// - you want to implement a different retry loop
+final public class NeverRetry: RetryPolicy {
+  public init() {}
+
+  public func onError(state: RetryState, error: RequestError) -> RetryResult {
+    .exhausted(error)
+  }
+
+  public func onThrottle(state: RetryState, error: RequestError) -> ThrottleResult {
+    .exhausted(error)
+  }
+}
