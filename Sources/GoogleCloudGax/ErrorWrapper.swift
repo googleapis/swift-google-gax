06bb6b512b60ba935022019ffb2b8f1492686ce2
Carlos O'Ryan <coryan@google.com>
2026-05-29 13:22:57 -0400

    feat(gax): add helper to parse errors (#209)
    When received over HTTP+JSON service errors have a slight odd encoding:
    they are contained in a message with a single `error` field. This PR
    introduces a helper to parse the errors in this format.

---
+++ Sources/GoogleCloudGax/ErrorWrapper.swift
@@ -0,0 +1,59 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
+import GoogleCloudWkt
+import GoogleRpc
+
+/// The services send errors using this structure.
+struct ErrorWrapper: Decodable {
+  init?(data: Data, response: HTTPURLResponse) {
+    let decoder = GoogleCloudWkt._ProtoJSONDecoder()
+    guard let w = try? decoder.decode(Self.self, from: data) else {
+      return nil
+    }
+    self = w
+  }
+
+  let error: WrappedStatus
+
+  struct WrappedStatus: Decodable {
+    /// The HTTP status code.
+    let code: Int32
+    /// The gRPC status code in string form.
+    let status: String?
+    /// The error message, if any.
+    let message: String
+    /// The sequence of error details, wrapped as anys. May be empty or omitted.
+    let details: [GoogleCloudWkt.`Any`]
+  }
+}
+
+extension ServiceError {
+  /// Create a a new `ServiceDetails`.
+  init(
+    wrapper: ErrorWrapper,
+  ) {
+    if let s = wrapper.error.status, let code = GoogleRpc.Code.init(stringValue: s) {
+      self.code = code
+    } else {
+      self.code = .unknown
+    }
+    self.message = wrapper.error.message
+    self.details = wrapper.error.details.map { StatusDetail(from: $0) }
+  }
+}
---
+++ Tests/ErrorWrapperTest.swift
@@ -0,0 +1,173 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
+import Testing
+@testable import GoogleCloudGax
+import GoogleRpc
+
+@Suite struct ErrorWrapperTests {
+  @Test func validStatus() throws {
+    let json = """
+      {
+        "error": {
+          "code": 400,
+          "status": "INVALID_ARGUMENT",
+          "message": "invalid argument message",
+          "details": []
+        }
+      }
+      """.data(using: .utf8)!
+
+    let response = HTTPURLResponse(
+      url: URL(string: "http://example.com")!, statusCode: 400, httpVersion: nil, headerFields: nil)!
+
+    guard let wrapper = ErrorWrapper(data: json, response: response) else {
+      Issue.record("Failed to decode ErrorWrapper")
+      return
+    }
+
+    let serviceError = ServiceError(wrapper: wrapper)
+    #expect(serviceError.code == .invalidArgument)
+    #expect(serviceError.message == "invalid argument message")
+  }
+
+  @Test func nilStatus() throws {
+    let json = """
+      {
+        "error": {
+          "code": 500,
+          "message": "internal error message",
+          "details": []
+        }
+      }
+      """.data(using: .utf8)!
+
+    let response = HTTPURLResponse(
+      url: URL(string: "http://example.com")!, statusCode: 500, httpVersion: nil, headerFields: nil)!
+
+    guard let wrapper = ErrorWrapper(data: json, response: response) else {
+      Issue.record("Failed to decode ErrorWrapper")
+      return
+    }
+
+    let serviceError = ServiceError(wrapper: wrapper)
+    #expect(serviceError.code == .unknown)
+    #expect(serviceError.message == "internal error message")
+  }
+
+  @Test func unknownStatus() throws {
+    let json = """
+      {
+        "error": {
+          "code": 500,
+          "status": "SOME_UNKNOWN_STATUS",
+          "message": "unknown status message",
+          "details": []
+        }
+      }
+      """.data(using: .utf8)!
+
+    let response = HTTPURLResponse(
+      url: URL(string: "http://example.com")!, statusCode: 500, httpVersion: nil, headerFields: nil)!
+
+    guard let wrapper = ErrorWrapper(data: json, response: response) else {
+      Issue.record("Failed to decode ErrorWrapper")
+      return
+    }
+
+    let serviceError = ServiceError(wrapper: wrapper)
+    #expect(serviceError.code == .unknown)
+    #expect(serviceError.message == "unknown status message")
+  }
+
+  @Test func missingDetails() throws {
+    let json = """
+      {
+        "error": {
+          "code": 400,
+          "status": "INVALID_ARGUMENT",
+          "message": "missing details message"
+        }
+      }
+      """.data(using: .utf8)!
+
+    let response = HTTPURLResponse(
+      url: URL(string: "http://example.com")!, statusCode: 400, httpVersion: nil, headerFields: nil)!
+
+    guard let wrapper = ErrorWrapper(data: json, response: response) else {
+      Issue.record("Failed to decode ErrorWrapper")
+      return
+    }
+
+    let serviceError = ServiceError(wrapper: wrapper)
+    #expect(serviceError.code == .invalidArgument)
+    #expect(serviceError.message == "missing details message")
+    #expect(serviceError.details.isEmpty)
+  }
+
+  @Test func withHelpDetails() throws {
+    let json = """
+      {
+        "error": {
+          "code": 400,
+          "status": "INVALID_ARGUMENT",
+          "message": "message with help details",
+          "details": [
+            {
+              "@type": "type.googleapis.com/google.rpc.Help",
+              "links": [
+                {
+                  "description": "Google developers console API activation",
+                  "url": "https://console.developers.google.com/apis/api/telcoautomation.googleapis.com/overview?project=test-only-project"
+                }
+              ]
+            }
+          ]
+        }
+      }
+      """.data(using: .utf8)!
+
+    let response = HTTPURLResponse(
+      url: URL(string: "http://example.com")!, statusCode: 400, httpVersion: nil, headerFields: nil)!
+
+    guard let wrapper = ErrorWrapper(data: json, response: response) else {
+      Issue.record("Failed to decode ErrorWrapper")
+      return
+    }
+
+    let serviceError = ServiceError(wrapper: wrapper)
+    #expect(serviceError.code == .invalidArgument)
+    #expect(serviceError.message == "message with help details")
+    #expect(serviceError.details.count == 1)
+
+    let wantDetails: [StatusDetail] = [
+      .help(
+        Help(
+          links: [
+            Help.Link(
+              description: "Google developers console API activation",
+              url:
+                "https://console.developers.google.com/apis/api/telcoautomation.googleapis.com/overview?project=test-only-project"
+            )
+          ]
+        ))
+    ]
+
+    #expect(serviceError.details == wantDetails)
+  }
+}
