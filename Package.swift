c3ac9bea6f4d66ec5852890bee208499a3fd3dbb
Jeff Ching <chingor@google.com>
2026-09-23 20:13:22 +0000

    chore: release circa 2026-09-23 (#1099)
    Bump library versions to 0.3.0 and regenerate client libraries for
    release.

--- Package.swift
+++ Package.swift
@@ -33,17 +33,17 @@ let package = Package(
     localOrRemotePackage(
       url: "https://github.com/googleapis/swift-google-auth",
       path: "pkgs/swift-google-auth",
-      from: "0.2.0"
+      from: "0.3.0"
     ),
     localOrRemotePackage(
       url: "https://github.com/googleapis/swift-google-wkt",
       path: "pkgs/swift-google-wkt",
-      from: "0.2.0"
+      from: "0.3.0"
     ),
     localOrRemotePackage(
       url: "https://github.com/googleapis/swift-google-rpc",
       path: "generated/swift-google-rpc",
-      from: "0.2.0"
+      from: "0.3.0"
     ),
     .package(url: "https://github.com/apple/swift-log", from: "1.14.0"),
     .package(url: "https://github.com/apple/swift-collections", from: "1.6.0"),
